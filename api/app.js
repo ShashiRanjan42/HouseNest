07583a1b77cf8cdf40085cc7682f46962c1f5f16
ShashiRanjan42 <ranjanshashi323@gmail.com>
2024-05-31 14:15:00 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -13,12 +13,10 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-// Middleware
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
@@ -26,24 +24,8 @@ app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ message: "Something went wrong!" });
-});
+// console.log("test2");
 
-// Start the server
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is Runing");
 });
-
-// Graceful shutdown
-const gracefulShutdown = () => {
-  server.close(() => {
-    console.log("Server closed");
-    process.exit(0);
-  });
-};
-
-process.on("SIGTERM", gracefulShutdown);
-process.on("SIGINT", gracefulShutdown);
