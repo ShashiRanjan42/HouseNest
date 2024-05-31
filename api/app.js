903743861970a25bef91be92cd65c6e1a457152c
ShashiRanjan42 <ranjanshashi323@gmail.com>
2024-05-31 14:07:15 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import postRoute from "./routes/postRoute.js";
 import authRoute from "./routes/authRoute.js";
 import testRoute from "./routes/testRoute.js";
@@ -8,12 +9,16 @@ import userRoute from "./routes/userRoute.js";
 import chatRoute from "./routes/chatRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 
+dotenv.config(); // Load environment variables from .env file
+
 const app = express();
 
+// Middleware
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
@@ -21,8 +26,24 @@ app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-// console.log("test2");
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ message: "Something went wrong!" });
+});
 
-app.listen(3000, () => {
+// Start the server
+const server = app.listen(3000, () => {
   console.log("Server is Runing");
 });
+
+// Graceful shutdown
+const gracefulShutdown = () => {
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
