4e515358f32acaca1036181913aa0fa0bb8b2ef7
ShashiRanjan42 <ranjanshashi323@gmail.com>
2024-05-31 14:27:27 +0530

    commit

--- socket/app.js
+++ socket/app.js
@@ -1,44 +1,53 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
+
+const PORT = 4000; // Define the port number
 
 const io = new Server({
-    cors: {
-      origin: "https://house-nest.vercel.app",
-    },
-  });
+  cors: {
+    origin: "https://house-nest.vercel.app",
+  },
+});
 
-let onlineUser = [];
+let onlineUsers = [];
 
 const addUser = (userId, socketId) => {
-    const userExits = onlineUser.find((user) => user.userId === userId);
-    if(!userExits) {
-        onlineUser.push({ userId, socketId});
-    }
+  const userExists = onlineUsers.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUsers.push({ userId, socketId });
+  }
 };
 
-// jb tab close krenge tb socket ko htana hoga
 const removeUser = (socketId) => {
-    onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
 };
 
-// find user
 const getUser = (userId) => {
-    return onlineUser.find((user) => user.userId !== userId);
+  return onlineUsers.find((user) => user.userId === userId);
 };
 
 io.on("connection", (socket) => {
-    socket.on("newUser", (userId) => {
-        addUser(userId, socket.id);
-    });
+  console.log("User connected:", socket.id);
 
-    socket.on("sendMessage", ({ receiverId, data }) => {
-        const receiver = getUser(receiverId);
-        io.to(receiver.socketId).emit("getMessage", data);
-    });
+  socket.on("newUser", (userId) => {
+    console.log("New user joined:", userId);
+    addUser(userId, socket.id);
+  });
 
-    socket.on("disconnect", () => {
-        removeUser(socket.id);
-    });
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    } else {
+      console.log("User not found:", receiverId);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    removeUser(socket.id);
+  });
 });
 
-
-io.listen("4000");
+io.listen(PORT, () => {
+  console.log(`Socket.io server listening on port ${PORT}`);
+});
