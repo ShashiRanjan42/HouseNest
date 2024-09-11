306d386d160ca66c318e6907c2a27c2baf6d0e4d
ShashiRanjan42 <ranjanshashi323@gmail.com>
2024-09-11 15:40:30 +0530

    Commit

--- api/app.js
+++ api/app.js
@@ -13,15 +13,7 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    // baseURL:"http://localhost:3000/api",
-    baseURL:"https://housenest.onrender.com/api",
+    baseURL:"http://localhost:3000/api",
+    // baseURL:"https://housenest.onrender.com/api",
     withCredentials: true,
 });
 
