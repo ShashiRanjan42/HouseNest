ecb78b329089d331e1446a8762cf31b09ff3b28b
ShashiRanjan42 <101126472+ShashiRanjan42@users.noreply.github.com>
2024-09-16 10:00:12 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:3000/api",
+    // baseURL:"http://localhost:3000/api",
     // baseURL:"https://housenest.onrender.com/api",
+    baseURL:"/api",
     withCredentials: true,
 });
 
