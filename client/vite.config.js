3c655f10d131c4b26de8a922bc45e000b947f2cf
ShashiRanjan42 <101126472+ShashiRanjan42@users.noreply.github.com>
2024-09-16 10:01:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://housenest.onrender.com',
+      // '/api': 'http://localhost:3000',
+        
+    },
+  },
+  plugins: [react()],
 })
